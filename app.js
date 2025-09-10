d9eada892025f2b118b3b865670a66f0238445d1
oerraoui <itsscarl2022@gmail.com>
2025-09-10 15:37:36 +0100

    improve player movement | add request animation frame

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const p1 = document.getElementById("player")
 const g = document.getElementById("game")
 let pos = 135
-document.addEventListener("keydown" , (e)=> {
-    if(e.key === "ArrowLeft" && pos > 0){
+let Lp = false
+let Lr = false
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft") {
+        Lp = true
+    } else if(e.key === "ArrowRight") {
+        Lr = true
+    }
+})
+
+document.addEventListener("keyup", (e) => {
+    if(e.key === "ArrowLeft") {
+        Lp = false
+    } else if(e.key === "ArrowRight") {
+        Lr = false
+    }
+})
+
+
+function update() {
+    if(Lp && pos > 0) {
         pos -= 5
-    } else if(e.key === "ArrowRight" && pos < g.clientWidth - p1.clientWidth){
+    } else if(Lr && pos < g.clientWidth - p1.clientWidth) {
         pos += 5
     }
+    
     p1.style.left = pos + "px"
-})
+    
+    requestAnimationFrame(update)
+}
+requestAnimationFrame(update)
